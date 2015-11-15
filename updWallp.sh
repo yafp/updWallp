46b963a0ad4862f4123fb756c2ffce4909f54aaf
yafp <fidel@yafp.de>
2015-11-15 15:32:45 +0100

    20151115 - Several changes - many functions moved to external files

--- updWallp.sh
+++ updWallp.sh
@@ -1,34 +1,20 @@
 #!/bin/bash
 #
 #  Name:       updWallp
-#  Version:    0.2
+#  Version:    0.3
 #  Function:   Script to pick a random image from a folder, generate a dimmed & blured version of it and set it as wallpaper
 #  Usage:      ./updWallp.sh /path/to/yourImageSourceFolder
 #  Github:     https://github.com/yafp/updWallp
 #
-#
-#  Related:
-#              - https://github.com/romannurik/muzei/
-#              - https://github.com/aepirli/linmuzei
-
 
 
 # ---------------------------------------------------------------------
-# CONFIGURATION
+# INCLUDE MAIN CONFIG
 # ---------------------------------------------------------------------
-updWallpDir=""                                          # Define where the updWallp folder is located- Must be configured
+source config.sh
+source functions.sh
 
 imageSourcePath=$1                                    # example: "/home/foo/Pictures"
-backupFilename="currentBaseWallpaper.png"             # filename for backup copy of selected file
-outputFilename="currentGeneratedWallpaper.png"        # filename for generated file
-
-blurCommand="-channel RGBA  -blur 0x9"                # imageMagick blur command example:    -channel RGBA  -blur 0x8
-dimCommand="-brightness-contrast -30x10"              # imageMagick dim command example:     -brightness-contrast -30x10
-                                                      #   where -30 is to darken by 30 and +10 is to increase the contrast by 10.
-notifyPath="/usr/bin/notify-send"
-bold=$(tput bold)                                     # cli output in bold
-normal=$(tput sgr0)                                   # cli output in normal
-
 
 
 # ---------------------------------------------------------------------
@@ -39,24 +25,6 @@ function checkImageMagick() {
    command -v convert >/dev/null 2>&1 || { echo >&2 "Imagemagick is required but not installed.  Aborting."; exit 1; }
    printf "\t\t\t\t\t\t\t\t\t${bold}OK${normal}\n"
 }
-
-
-# ---------------------------------------------------------------------
-# CHECKUPDWALLP
-# ---------------------------------------------------------------------
-function checkupdWallpFolder() {
-   printf "...Checking updWallp working dir\n"
-   if [ -d "$updWallpDir" ];                                                    # if script folder is defined
-      then
-      printf "...updWallp folder:\t\t$updWallpDir \t\t${bold}OK${normal}\n"      # can continue
-   else
-      displayNotification "updWallp" "updWallp folder not configured"
-      printf "${bold}ERROR:${normal} updWallp folder not configured\n"
-      printf "${bold}...aborting now${normal}"
-      exit                                                              # otherwise die
-   fi
-}
-
 
 
 
@@ -77,69 +45,14 @@ function checkImageSourceFolder() {
 
 
 
-# ---------------------------------------------------------------------
-# NOTIFICATION
-# ---------------------------------------------------------------------
-function displayNotification() {
-   if [ -f $notifyPath ];    # if notify-send exists
-   then
-      notify-send "$1" "$2" -i "$updWallpDir/img/appIcon_128px.png"
-   else
-      printf "${bold}WARNING:${normal} notify-send not found\n"
-   fi
-}
-
-
 
 # ---------------------------------------------------------------------
 # GENERATE CURRENT IMAGES IN WORKING DIR
 # ---------------------------------------------------------------------
 generateNewWallpaper(){
    randomImage=$(find $imageSourcePath -type f | shuf -n 1)             # pick random image from source folder
-   convert "$randomImage" $backupFilename                              # copy random base image to project folder
+   convert "$randomImage" $backupFilename                               # copy random base image to project folder
    convert "$randomImage" $blurCommand $dimCommand  $outputFilename     # Create a dimmed & blur-verion of the image into the working dir
-}
-
-
-
-# ---------------------------------------------------------------------
-# SETLINUXWALLPAPER
-# ---------------------------------------------------------------------
-function setLinuxWallpaper() {
-   printf "...Trying to activate the new wallpaper\n"
-
-   if [ "$(pidof gnome-settings-daemon)" ]
-     then
-       printf "...Setting wallpaper using gsettings\t\t\t\t\t\t\t\t${bold}OK${normal}\n"
-       gsettings set org.gnome.desktop.background picture-uri file://$updWallpDir/$outputFilename
-       displayNotification "updWallp" "Enjoy your new wallpaper"
-     else
-       if [ -f ~/.xinitrc ]
-       then
-         if [ "$(which feh)" ]
-         then
-           printf "Gnome-settings-daemons not running, setting wallpaper with feh..."
-           feh $outputFilename
-           feh_xinitSet
-         elif [ "$(which hsetroot)" ]
-         then
-           printf "Gnome-settings-daemons not running, setting wallpaper with hsetroot..."
-           hsetroot -cover $outputFilename
-           hsetroot_xinitSet
-         elif [ "$(which nitrogen)" ]
-         then
-           printf "Gnome-settings-daemons not running, setting wallpaper with nitrogen..."
-           nitrogen $outputFilename
-           nitrogen_xinitSet
-         else
-           printf "You need to have either feh, hsetroot or nitrogen, bruhbruh."
-           exit
-         fi
-       else
-         printf "You should have a ~/.xinitrc file."
-         exit
-       fi
-     fi
 }
 
 
@@ -148,12 +61,14 @@ function setLinuxWallpaper() {
 # SCRIPT-LOGIC
 # #####################################################################
 clear
+cd $updWallpDir
+
 printf "${bold}*** updWallp ***${normal}\n\n"
 printf "...Operating system:\t\t $OSTYPE\t\t\t\t\t\t\t${bold}OK${normal}\n"
 
-checkImageMagick           # function to check if ImageMagick is installed
-checkupdWallpFolder        # function to check if user configured the script folder
-checkImageSourceFolder     # check if user-supplied source folder exists
+checkImageMagick                             # function to check if ImageMagick is installed
+checkupdWallpFolder                          # function to check if user configured the script folder
+checkImageSourceFolder                       # check if user-supplied source folder exists
 
-generateNewWallpaper       # generates a new wallpaper
-setLinuxWallpaper          # set the linux wallpaper
+generateNewWallpaper                         # generates a new wallpaper
+setLinuxWallpaper  "$outputFilename"         # set the linux wallpaper
