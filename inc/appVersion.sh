2eb0531a2fd8c66d28319ffcc3b382b0f070863d
yafp <f.poeck@kniel.de>
2015-11-27 14:02:23 +0100

    20151127 - Moved several variables from config.php to individual files in inc subfolder

---
+++ inc/appVersion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#  Name:       appVersion.sh
+#  Function:   Defines only the version of updWallp
+#  Github:     https://github.com/yafp/updWallp
+#
+
+# ---------------------------------------------------------------------
+# APP VERSION - DONT TOUCH
+# ---------------------------------------------------------------------
+
+# AppVersion
+appVersion=0.7.8
---
+++ inc/colorDefinitions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#  Name:       appVersion.sh
+#  Function:   Defines the color-codes for terminal output of updWallp
+#  Github:     https://github.com/yafp/updWallp
+#
+
+# ---------------------------------------------------------------------
+# COLOR DEFINITIONS - DONT TOUCH
+# ---------------------------------------------------------------------
+
+# format
+bold=$(tput bold)
+normal=$(tput sgr0)
+blink=$(tput blink)
+reverse=$(tput smso)
+underline=$(tput smul)
+
+# colors
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+lime_yellow=$(tput setaf 190)
+powder_blue=$(tput setaf 153)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+bright=$(tput bold)
