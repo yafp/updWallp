f1e910fff6ac7912b7a030edc72e305f1aa63ed0
yafp <f.poeck@kniel.de>
2015-11-27 14:03:00 +0100

    20151127 - added loader.sh which is responsible for sourcing all needed .sh files in the 2 main scripts

---
+++ inc/loader.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#  Name:       appVersion.sh
+#  Function:   Defines only the version of updWallp
+#  Github:     https://github.com/yafp/updWallp
+#
+
+# ---------------------------------------------------------------------
+# LOAD ALL FILES FROM INC FOLDER
+# ---------------------------------------------------------------------
+source config.sh 				# load the config file
+
+source inc/functions.sh # load functions
+source inc/appVersion.sh # load app version variable
+source inc/colorDefinitions.sh # load color definition variables
