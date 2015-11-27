da23d934089d17ab50e71c221260d203c9d2698f
yafp <f.poeck@kniel.de>
2015-11-27 14:01:37 +0100

    20151127 - Moving functions to inc subfolder

--- inc/functions.sh
+++ inc/functions.sh
@@ -25,19 +25,64 @@ function checkOperatingSystem()
 {
    if [[ $OSTYPE = *linux* ]] # we are on linux - so continue checking
       then
-         printf "${bold}${green}OK${normal} ... Operating system: $OSTYPE\n"
+      printf "${bold}${green}OK${normal} ... Operating system: $OSTYPE\n"
+      checkLinuxDesktopEnvironment # Check which desktop environment is in use
 
-         if [ "$(pidof gnome-settings-daemon)" ]; # found gsettings - which is needed to set the wallpaper
+   else # not linux -> not supported
+      printf "${bold}${red}ERROR${normal} ... Unexpected operating system: $OSTYPE Aborting\n"
+      exit
+   fi
+}
+
+
+
+# ---------------------------------------------------------------------
+# Checking the Desktop environment
+# ---------------------------------------------------------------------
+function checkLinuxDesktopEnvironment()
+{
+   desktopEnv=$DESKTOP_SESSION
+
+   # possible answers:
+   #
+   # - gnome 3    = gnome
+   # - xfce4      = xfce4
+   # - KDE        = ?
+
+   case  $desktopEnv  in
+      "gnome")
+         # Is Supported - Nothing to do here
+         if [ "$(which gnome-session)" ]
             then
-               printf "${bold}${green}OK${normal} ... Found gsettings\n"
-         else # big problem - we cant see the wallpaper on non-gnome setups so far
-            printf "${bold}${red}ERROR${normal} ... Setting wallpaper is currently only supported on Gnome (using: gsettings / gnome-settings-daemon). Aborting\n\n"
+            gnomeVersion="$(gnome-session --version)" # Get Gnome Version
+            printf "${bold}${green}OK${normal} ... Detected: ${gnomeVersion}\n"
+
+            # Gnome 3.x?
+            if [[ $gnomeVersion == *"gnome-session 3."* ]]
+               then
+               printf "${bold}${green}OK${normal} ... Gnome 3 is supported\n"
+               return
+            fi
+
+            # Gnome 2.x?
+            if [[ $gnomeVersion == *"gnome-session 2."* ]]
+               then
+               printf "${bold}${green}OK${normal} ... Gnome 2 is supported\n"
+               return
+            fi
+
+            # if this code is reached - it seems to be gnome - but beither Gnome2 nor Gnome3 - Unexpected - exit
+            printf "${bold}${red}ERROR${normal} ... Unsupported Gnome Version. Aborting\n"
             exit
          fi
-   else # not linux -> not supported
-         printf "${bold}${red}ERROR${normal} ... Unexpected operating system: $OSTYPE Aborting\n"
+         ;;
+
+      *)
+         printf "${bold}${red}ERROR${normal} ... Unsupported Desktop Envorinment detected ($desktopEnv).\n"
+         printf "${bold}${red}ERROR${normal} ... Currently only Gnome 2 & 3 are supported.\n"
+         printf "${bold}${red}ERROR${normal} ... More: ${underline}https://github.com/yafp/updWallp/wiki/Supported-Desktop-Environments${normal}. Aborting\n"
          exit
-   fi
+   esac
 }
 
 
@@ -47,7 +92,7 @@ function checkOperatingSystem()
 # ---------------------------------------------------------------------
 function checkImageMagick() {
    if [ "$(which convert)" ]
-   then
+      then
       printf "${bold}${green}OK${normal} ... Found ImageMagick\n"
    else
       printf "${bold}${red}ERROR${normal} ... ImageMagick not found\n"
@@ -68,19 +113,19 @@ function checkRemoteRequirements()
 {
    # check for curl
    if [ "$(which curl)" ]
-   then
-      printf "${bold}${green}OK${normal} ... Found cURL (Muzei-Mode)\n"
+      then
+      printf "${bold}${green}OK${normal} ... Found cURL (remote-mode)\n"
    else
-      printf "${bold}${red}ERROR${normal} ... cURL not found (Muzei-Mode). Aborting\n"
+      printf "${bold}${red}ERROR${normal} ... cURL not found (remote-mode). Aborting\n"
       exit
    fi
 
    # check for jq
    if [ "$(which jq)" ]
-   then
-      printf "${bold}${green}OK${normal} ... Found JQ (Muzei-Mode)\n"
+      then
+      printf "${bold}${green}OK${normal} ... Found JQ (remote-mode)\n"
    else
-      printf "${bold}${red}ERROR${normal} ... JQ not found (Muzei-Mode). Aborting\n"
+      printf "${bold}${red}ERROR${normal} ... JQ not found (remote-mode). Aborting\n"
       exit
    fi
 }
@@ -88,16 +133,16 @@ function checkRemoteRequirements()
 
 
 # ---------------------------------------------------------------------
-# NOTIFICATION
+# NOTIFICATION (enable or disable via config.sh)
 # ---------------------------------------------------------------------
 function displayNotification() {
    if [ "$enableNotifications" = true ]      # If notifications are enabled at all
       then
       if [ -f $notifyPath ];    # if notify-send exists
-      then
+         then
          $notifyPath "$1" "$2" -i "$updWallpDir/img/appIcon_128px.png"
       else
-         printf "${bold}${yellow}WARNING${normal} ... notify-send not found\n"
+         printf "${bold}${yellow}WARNING${normal} ... Unable to display a notification (notify-send not found)\n"
       fi
    fi
 }
@@ -109,15 +154,41 @@ function displayNotification() {
 # SETLINUXWALLPAPER
 # ---------------------------------------------------------------------
 function setLinuxWallpaper() {
-   if [ "$(pidof gnome-settings-daemon)" ];
+   if [ "$(which gnome-session)" ]
       then
-         /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://$updWallpDir/$1
-         displayNotification "updWallp" "Wallpaper updated"
-         printf "${bold}${green}OK${normal} ... Wallpaper set via gsettings\n"
-   else
-        printf "${bold}${red}ERROR${normal} ... Sorry dude but your system is not supported.\n"
-        printf "${bold}${red}ERROR${normal} ... Currently only Gnome is supported (using: gsettings)\n"
-        exit
+      gnomeVersion="$(gnome-session --version)" # Get Gnome Version
+
+      # Gnome 3.x?
+      if [[ $gnomeVersion == *"gnome-session 3."* ]]
+         then
+         #printf "${bold}${green}OK${normal} ... Gnome 3\n"
+         if [ "$(pidof gnome-settings-daemon)" ];
+            then
+            /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://$updWallpDir/$1
+            displayNotification "updWallp" "Wallpaper updated (using gsettings on Gnome 3)"
+            printf "${bold}${green}OK${normal} ... Wallpaper updated (using gsettings on Gnome 3)\n"
+            return
+         fi
+      fi
+
+      # Gnome 2.x?
+      if [[ $gnomeVersion == *"gnome-session 2."* ]]
+         then
+         gconftool-2 --type=string --set /desktop/gnome/background/picture_filename $updWallpDir/$1
+         displayNotification "updWallp" "Wallpaper updated (using gconftool on Gnome 2)"
+         printf "${bold}${green}OK${normal} ... Wallpaper updated (using gconftool on Gnome 2)\n"
+         return
+      fi
+
+      # if this code is reached - it seems to be gnome - but neither Gnome2 nor Gnome3 - Unexpected - exit
+      printf "${bold}${red}ERROR${normal} ... Unsupported Gnome Version. Aborting\n"
+      exit
+
+   else # its for sure not gnome which is used here
+      printf "${bold}${red}ERROR${normal} ... Sorry dude but your system is not supported.\n"
+      printf "${bold}${red}ERROR${normal} ... Currently only Gnome is supported (using: gsettings)\n"
+      printf "${bold}${red}ERROR${normal} ... More: ${underline}https://github.com/yafp/updWallp/wiki/Supported-Desktop-Environments${normal}. Aborting\n"
+      exit
    fi
 }
 
@@ -131,10 +202,10 @@ function setLinuxWallpaper() {
 function checkImageSourceFolder() {
    if [ -d "$localUserImageFolder" ];                                                              # if image source folder exists
       then
-      printf "${bold}${green}OK${normal} ... Image folder: $localUserImageFolder is valid\n"       # can continue
+      printf "${bold}${green}OK${normal} ... Image source folder: ${underline}$localUserImageFolder${normal} is valid\n"       # can continue
       getNewRandomLocalFilePath                                                  # get a new local filepath
    else
-      printf "${bold}${red}ERROR${normal} ... Local mode but defined image dir ($localUserImageFolder) isnt a valid directory. Aborting\n"      # can continue
+      printf "${bold}${red}ERROR${normal} ... Local mode but image source dir ${underline}$localUserImageFolder${normal} isnt a valid directory. Aborting\n"      # can continue
       exit
    fi
 }
@@ -149,20 +220,23 @@ function getRemoteMuzeiImage()
    if ! [ -f ./muzeich.json ]
       then
       curl -o muzeich.json 'https://muzeiapi.appspot.com/featured?cachebust=1'
+      printf "${bold}${green}OK${normal} ... Downloaded muzei.json (initial).\n"
    else
       curl -o muzeich2.json 'https://muzeiapi.appspot.com/featured?cachebust=1'
-      if [ "$(cmp muzeich.json muzeich2.json)" ]
+      printf "${bold}${green}OK${normal} ... Downloaded muzei.json (checking for changes).\n"
+      if [ "$(cmp muzeich.json muzeich2.json)" ] # there is a new muzei image available
          then
          mv muzeich2.json muzeich.json
          printf "${bold}${green}OK${normal} ... There is a new Muzei image available. Loading it.\n"
-      else
-         rm muzeich2.json
+
+      else # muzei offers still the same image
+         rm muzeich2.json # remove the second json file
          printf "${bold}${green}OK${normal} ... There is no new Muzei image available. Nothing to do here.\n"
-         #exit
+         exit
       fi
    fi
 
-   # parse the Muzei JSON
+   # parse the Muzei JSON to extract the new image url
    imageUri=`jq '.imageUri' $updWallpDir/muzeich.json | sed s/\"//g`
    imageFile=`basename $imageUri`
    title=`jq '.title' $updWallpDir/muzeich.json | sed s/\"//g`
@@ -187,7 +261,7 @@ function getRemoteMuzeiImage()
 function getNewRandomLocalFilePath()
 {
    newImage=$(find $localUserImageFolder -type f | shuf -n 1)             # pick random image from source folder
-   printf "${bold}${green}OK${normal} ... Selected a new random image from local folder\n"
+   printf "${bold}${green}OK${normal} ... Random local image choosen\n"
 }
 
 
@@ -229,9 +303,10 @@ function cleanupUpdWallpDir()
       rm $imageFile
    fi
 
-   if [ -f "$muzeiFilename" ]
-      then
-      rm $muzeiFilename
-   fi
-   printf "${bold}${green}OK${normal} ... Finished cleaning up $updWallpDir\n"
+   #if [ -f "$muzeiFilename" ]
+      #then
+      #rm $muzeiFilename
+   #fi
+
+   printf "${bold}${green}OK${normal} ... Finished cleaning up ${underline}$updWallpDir${normal}\n"
 }
