4faa2db5cb9b4331973f14564539c4a18263c7c1
yafp <f.poeck@kniel.de>
2015-11-30 14:38:14 +0100

    20151130 - loader checks now if sourcing makes sense or not (checks if file exists before sourcing)

--- inc/loader.sh
+++ inc/loader.sh
@@ -8,8 +8,45 @@
 # ---------------------------------------------------------------------
 # LOAD ALL FILES FROM INC FOLDER
 # ---------------------------------------------------------------------
-source config.sh 				# load the config file
 
-source inc/functions.sh # load functions
-source inc/appVersion.sh # load app version variable
-source inc/colorDefinitions.sh # load color definition variables
+# source config.sh if possible
+if [ -f config.sh ]
+then
+   source config.sh 				# load the config file
+else
+   printf "updWallp - Can not source config.sh. Aborting"
+   exit
+fi
+
+
+
+# source inc/functions.sh if possible
+if [ -f inc/functions.sh ]
+then
+   source inc/functions.sh 				# load  functions.sh
+else
+   printf "updWallp - Can not source inc/functions.sh. Aborting"
+   exit
+fi
+
+
+
+# source inc/appVersion.sh if possible
+if [ -f inc/appVersion.sh ]
+then
+   source inc/appVersion.sh 				# load appVersion.sh
+else
+   printf "updWallp - Can not source inc/appVersion.sh. Aborting"
+   exit
+fi
+
+
+
+# source inc/colorDefinitions.sh if possible
+if [ -f inc/colorDefinitions.sh ]
+then
+   source inc/colorDefinitions.sh 				# load colorDefinitions.sh
+else
+   printf "updWallp - Can not source inc/colorDefinitions.sh. Aborting"
+   exit
+fi
