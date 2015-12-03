8086ca8bc7bf9435c64f47bff66b4ec328e0a147
yafp <fidel@yafp.de>
2015-12-03 23:12:09 +0100

    20151203 - Moving to 0.7.12 (installationPath is now only configured once -> in config.sh)

--- updWallp.sh
+++ updWallp.sh
@@ -12,80 +12,81 @@
 
 
 # ---------------------------------------------------------------------
-# LOCAL CONFIG
+# LOCAL CONFIG - no need to touch
 # ---------------------------------------------------------------------
-updWallpDir=""              # define the folder where you copied the updWallp folder to (Example: /home/username/updWallp)
-
 muzeiFilename="muzeiImage.png"
 primaryParamter=$1
 localUserImageFolder=$2
 
 
 
-
 # #####################################################################
 # SCRIPT-LOGIC
 # #####################################################################
-clear
-if [ -d "$updWallpDir" ] # updWallp directory is set and valid
-	then
-	cd $updWallpDir # change to defined folder
+currentPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -f $currentPath/config.sh ]; then # found config file
+	source $currentPath/config.sh # load config file
 
-	source inc/loader.sh 		# source all needed files
+	# installationPath directory is set and valid
+	if [ -d "$installationPath" ]; then
+		cd $installationPath # change to defined folder
+		source inc/loader.sh 		# source all needed files
+		startUp
 
-	startUp
-
-	# Check if user supplied a parameter $1 to set a mode
-	#
-	if [ -z "$primaryParamter" ] # user did not supply $1
-		then
+		# Check if user supplied a parameter $1 to set a mode
+		if [ -z "$primaryParamter" ]; then
+			# user did not supply $1
 			printf "${bold}${red}ERROR${normal} ... you didnt supply any parameter. Try -h to get instructions\n"
 			exit
-	else # user supplied at least $1
+		else # user supplied at least $1
 
-		if [ "$primaryParamter" = "-h" ] || [ "$primaryParamter" = "-l" ] || [ "$primaryParamter" = "-r" ] # valid parameters
-			then
+			# valid parameters
+			if [ "$primaryParamter" = "-h" ] || [ "$primaryParamter" = "-l" ] || [ "$primaryParamter" = "-r" ]; then
 
-			if [ "$primaryParamter" = "-h" ] # parameter was -h
-				then
-					printf "\n${bold}Usage:${normal}\n"
-					printf "  Help:         ./updWallp.sh -h\n"
-					printf "  Local-Mode:   ./updWallp.sh -l /path/to/local/image/folder\n"
-					printf "  Remote-Mode:  ./updWallp.sh -r\n\n"
-					printf "${bold}Documentation:${normal}\n"
-					printf "  https://github.com/yafp/updWallp/wiki\n"
-					exit
-			fi
+				# parameter was -h
+				if [ "$primaryParamter" = "-h" ]; then
+						printf "\n${bold}Usage:${normal}\n"
+						printf "  Help:         ./updWallp.sh -h\n"
+						printf "  Local-Mode:   ./updWallp.sh -l /path/to/local/image/folder\n"
+						printf "  Remote-Mode:  ./updWallp.sh -r\n\n"
+						printf "${bold}Documentation:${normal}\n"
+						printf "  https://github.com/yafp/updWallp/wiki\n"
+						exit
+				fi
 
-			# for both: local and remote
-			checkOperatingSystem                         # check operating system
-			printf "${bold}${green}OK${normal} ... updWallp folder is set to: ${underline}$updWallpDir${normal}\n"
-			checkImageMagick                             # function to check if ImageMagick is installed
+				# for both: local and remote
+				checkOperatingSystem                         # check operating system
+				printf "${bold}${green}OK${normal} ... updWallp folder is set to: ${underline}$installationPath${normal}\n"
+				checkImageMagick                             # function to check if ImageMagick is installed
 
-			if [ "$primaryParamter" = "-l" ]; # parameter was -l = local mode
-				then
+				# parameter was -l = local mode
+				if [ "$primaryParamter" = "-l" ]; then
 					printf "${bold}${green}OK${normal} ... Using local-mode (-l)\n"
 					checkImageSourceFolder
-			fi
+				fi
 
-			if [ "$primaryParamter" = "-r" ] # parameter was -r = remote mode
-				then
+				# parameter was -r = remote mode
+				if [ "$primaryParamter" = "-r" ]; then
 					printf "${bold}${green}OK${normal} ... Using remote-mode (-r)\n"
 					checkRemoteRequirements
 					getRemoteMuzeiImage
-			fi
+				fi
 
-			generateNewWallpaper                         # generates a new wallpaper
-			setLinuxWallpaper  "$outputFilename"         # set the linux wallpaper
-			cleanupUpdWallpDir                           # Cleaning up
-			exit
-
-		else # parameter was not valid
-				printf "${bold}${red}ERROR${normal} Invalid parameter '$primaryParamter'\n\nStart with: '-h' for some basic instructions.\n"
+				generateNewWallpaper                         # generates a new wallpaper
+				setLinuxWallpaper  "$outputFilename"         # set the linux wallpaper
+				cleanupUpdWallpDir                           # Cleaning up
 				exit
+
+			else # parameter was not valid
+					printf "${bold}${red}ERROR${normal} Invalid parameter '$primaryParamter'\n\nStart with: '-h' for some basic instructions.\n"
+					exit
+			fi
 		fi
-	fi
-else # = updWallp directory is not set or not valid
-	printf "${bold}${red}ERROR${normal} updWallp folder not configured or not valid.\n"
-	exit                                                              # otherwise die
-fi # end of checking updWallp directory
+	else # = updWallp directory is not set or not valid
+		printf "${bold}${red}ERROR${normal} Variable 'installationPath' is not configured (in config.sh) or not valid. Aborting\n"
+		exit                                                              # otherwise die
+	fi # end of checking updWallp directory
+else
+	printf "${bold}${red}ERROR${normal} Unable to find 'config.sh'. Aborting\n"
+	exit
+fi
--- updWallpShowOrg.sh
+++ updWallpShowOrg.sh
@@ -12,43 +12,48 @@
 # ---------------------------------------------------------------------
 # LOCAL CONFIG
 # ---------------------------------------------------------------------
-updWallpDir=""        # define the folder where you copied the updWallp folder to
 toggleTime=10s                                  # how long the original image is displayed if user manually toggles script: updWallpShowOrg.sh
 
 
 # #####################################################################
 # SCRIPT-LOGIC
 # #####################################################################
-clear
-
-if [ -d "$updWallpDir" ] # if updWallpDir is configured and valid
-	then
-	cd $updWallpDir
-
-	source inc/loader.sh 	# load all inc files
-
-	printf "${bold}${green}OK${normal} ... updWallp folder is set to: ${underline}$updWallpDir${normal}\n"
-	startUp
-	if [ -z "$1" ];	# if user did not supply any paramter
-		then # do the normal script logic
+currentPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -f $currentPath/config.sh ]; then # found config file
+   source $currentPath/config.sh #load config file
+	# if installationPath is configured in config.sh and valid
+	if [ -d "$installationPath" ]; then
+		cd $installationPath
+		source inc/loader.sh 	# source the loader for all other inc files
+		printf "${bold}${green}OK${normal} ... updWallp folder is set to: ${underline}$installationPath${normal}\n"
+		startUp
+		#check if the user did supply any paramter or not
+		if [ -z "$1" ];then
+			# no parameter -> do the normal script logic
 			checkOperatingSystem                         # check operating system
 			setLinuxWallpaper "$backupFilename"        # set the linux wallpaper to the original file
 			printf "${bold}${green}OK${normal} ... Waiting for $toggleTime (seconds) until toggling back\n"
 			sleep "$toggleTime"
 			setLinuxWallpaper "$outputFilename"        # set the linux wallpaper back to the dimmed/blured version
-
-	else # user supplied a parameter
-		if [ "$1" = "-h" ] # check if parameter is valid: -h = help/documentation
-			then
+			exit
+		else # user supplied a parameter
+			# check if parameter is valid: -h = help/documentation
+			if [ "$1" = "-h" ]; then
 				printf "${bold}Usage:${normal}\n"
 				printf "  ./updWallpShowOrg.sh\n\n"
 				printf "${bold}Documentation:${normal}\n"
 				printf "  https://github.com/yafp/updWallp/wiki\n"
-		else
-			printf "${bold}${red}ERROR${normal} Invalid parameter '$1'\n\nStart with: '-h' for some basic instructions.\n"
+				exit
+			else
+				printf "${bold}${red}ERROR${normal} Invalid parameter '$1'\n\nStart with: '-h' for some basic instructions.\n"
+				exit
+			fi
 		fi
+	else		# updWallpDir is not configured
+		printf "${bold}${red}ERROR${normal} Variable 'installationPath' is not configured (in config.sh) or not valid. Aborting\n"
+		exit # die
 	fi
-else		# updWallpDir is not configured
-	printf "${bold}${red}ERROR${normal} Variable 'updWallpDir' is not not configured or not valid. Aborting\n"
-	exit # die
+else
+	printf "${bold}${red}ERROR${normal} Unable to find 'config.sh'. Aborting\n"
+	exit
 fi
