1990edf8f38f24aa26b60a8a2abc6096293d1582
yafp <f.poeck@kniel.de>
2015-12-04 15:10:06 +0100

    20151204 - Adding new inc files -> constants & errorStrings

---
+++ inc/constants.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#  Name:       constants.sh
+#  Function:   Defines some string constants we are using on several places
+#  Github:     https://github.com/yafp/updWallp
+#
+
+# ---------------------------------------------------------------------
+# CONSTANT STRINGS
+# ---------------------------------------------------------------------
+
+# AppVersion
+appName="updWallp"
+appDocURL="https://github.com/yafp/updWallp/wiki"
+
+
+# FILENAMES for ImageCreation
+#
+# local
+backupFilename="tmp/currentBaseWallpaper.png"             # filename for backup copy of selected file
+outputFilename="tmp/currentGeneratedWallpaper.png"        # generated Wallpaper which is set as wallpaper
+# remote
+muzeiFilename="tmp/muzeiImage.png"
